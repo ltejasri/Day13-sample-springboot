0ba8984a6fe695f2e76402686f40904a1e2d51a2
Tejasri <lingampally.tejasri@capgemini.com>
2018-10-03 11:02:58 -0400

    First commit

---
+++ src/main/java/com/capgemini/springboot/SpringbootApplication.java
@@ -0,0 +1,12 @@
+package com.capgemini.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApplication {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(SpringbootApplication.class, args);
+	}
+}
